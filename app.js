c62e3893bbc4db8c6941c0bdaf9a8129e9546da6
MuYhEt <pedrovzambi@outlook.com>
2017-11-01 11:50:57 -0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ bot.on("ready", function(message) {
 });
 
 bot.on("guildMemberAdd", function(member) {
-  member.guild.channels.find("name","main_chat").sendMessage(":new: | "+ member.toString() + " **Has been joined to our server!** Welcome to our server! Do not forget to read the "+"<#371434623701483520>"+" and the rules. If you have any questions, ask the support team! ");
+  member.guild.channels.find("name","main_chat").sendMessage(":new: | "+ member.toString() + " **Has been joined to our server!** Welcome to our server! Do not forget to read the "+"<#359050267960016906>"+" and the rules. If you have any questions, ask the support team! ");
   member.addRole(member.guild.roles.find("name","Community"));
 });
 
