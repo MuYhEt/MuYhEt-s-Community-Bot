42d76e4ce313e5d209a351640b70e3362d8fabd3
MuYhEt <pedrovzambi@outlook.com>
2017-11-01 12:03:45 -0200

    Update app.js

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const settings = require('./settings.json');
 
 ////////////////////////////////////////////////////////////////////////////////
 
-bot.on("ready", function(message) {
-  console.log("Ready");
-  bot.user.setGame("24/7 | MuYhEt's Community Bot");
+bot.on("ready", function() {
+console.log("Ready");
+bot.user.setGame("24/7 | v0.0.1 - "+settings.prefix+"help")
 });
 
 bot.on("guildMemberAdd", function(member) {
